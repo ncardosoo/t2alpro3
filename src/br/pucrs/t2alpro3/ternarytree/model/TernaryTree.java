066b6df303686b15053b47de65d76a213a3bb35c
ncardosoo <nelsoncardosoo@gmail.com>
2017-05-31 11:31:15 -0300

    Update TernaryTree.java

--- src/br/pucrs/t2alpro3/ternarytree/model/TernaryTree.java
+++ src/br/pucrs/t2alpro3/ternarytree/model/TernaryTree.java
@@ -3,32 +3,67 @@ package br.pucrs.t2alpro3.ternarytree.model;
 public class TernaryTree {
 
 	private Node root;
+	private LeftTree leftTree;
+	private RightTree rightTree;
 	
 	public TernaryTree() {
 		
 	}
 	
-	public TernaryTree(Node root) {
-		this.setRoot(root);
+	public TernaryTree(String entry) {
+		leftTree = new LeftTree();
+		rightTree = new RightTree();
+		this.getLeftTree().buildLeftTree(entry);
+		this.getRightTree().buildRightTree(entry);
 	}
 	
+	/**
+	 * 
+	 * @param leftTree
+	 * @param rightTree
+	 * @return an int that represents the size of the merged tree
+	 * 
+	 * TODO: build the merged tree
+	 */
 	public int merge(LeftTree leftTree, RightTree rightTree) {
-		// TODO Auto-generated method stub
-		return 3;
-	}
-
-	public Node getRoot() {
-		return root;
-	}
-
-	public void setRoot(Node root) {
-		this.root = root;
-	}
-
-	public void add(Node node) {
-		if(root == null) {
-			root = node;						
+		int res = -1;
+		
+		int longLeftPath = leftTree.getLongestCentralPath(leftTree.returnAllNodes(leftTree.getRoot()));
+		int longRightPath = rightTree.getLongestCentralPath(rightTree.returnAllNodes(rightTree.getRoot()));
+		
+		int leftTreeSize = leftTree.returnAllNodes(leftTree.getRoot()).size();
+		int rightTreeSize = rightTree.returnAllNodes(rightTree.getRoot()).size();
+		
+		/*
+		 * When the format of both tree is a simple line, in other words, they have just
+		 * central nodes, return the size of the longest tree
+		 */
+		if(longLeftPath == leftTreeSize && longRightPath == rightTreeSize) {
+			return leftTreeSize > rightTreeSize ? leftTreeSize : rightTreeSize;
 		}
+		
+		int maxSize = leftTreeSize + rightTreeSize;
+		int maxPath = longLeftPath > longRightPath ? longLeftPath : longRightPath;
+		
+		res = maxSize - maxPath;		
+		
+		return res;
+	}
+
+	public LeftTree getLeftTree() {
+		return leftTree;
+	}
+
+	public void setLeftTree(LeftTree leftTree) {
+		this.leftTree = leftTree;
+	}
+
+	public RightTree getRightTree() {
+		return rightTree;
+	}
+
+	public void setRightTree(RightTree rightTree) {
+		this.rightTree = rightTree;
 	}
 
 }
